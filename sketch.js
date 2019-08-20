bde311555d787d689a91fc901404aae4fdd82ef5
Diadem Shoukralla <diadem.shoukralla@mail.utoronto.ca>
2019-08-19 21:05:50 -0400

    Added welcome text to the default sketch

--- sketch.js
+++ sketch.js
@@ -4,4 +4,21 @@ function setup() {
   
   function draw() {
     background(220);
+
+    push();
+    var welcome = "Welcome";
+    textSize(100);
+    text(welcome, 10, 100);
+    fill(0, 102, 153);
+    text(welcome, 10, 150);
+    fill(0, 102, 153, 51);
+    text(welcome, 10, 200);
+    pop();
+
+    push();
+    var intro = "Use the top bar menu to try out an exercise!";
+    let time = millis();
+    textSize(30);
+    text(intro, 400, 300);
+    pop();
   }
