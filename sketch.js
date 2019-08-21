a3b90961c62bfd4fe897c09cde81f8f29e56b67e
Diadem Shoukralla <diadem.shoukralla@mail.utoronto.ca>
2019-08-20 21:12:16 -0400

    Rearranged code to prepare for dynamic rendering of sketches

--- sketch.js
+++ sketch.js
@@ -1,24 +1,31 @@
+var currentSketch;
+
+function getDefaultSketch(){
+  background(220);
+
+  push();
+  var welcome = "Welcome";
+  textSize(100);
+  text(welcome, 10, 100);
+  fill(0, 102, 153);
+  text(welcome, 10, 150);
+  fill(0, 102, 153, 51);
+  text(welcome, 10, 200);
+  pop();
+
+  push();
+  var intro = "Use the top bar menu to try out an exercise!";
+  let time = millis();
+  textSize(30);
+  text(intro, 10, 300);
+  pop();
+}
+
 function setup() {
     createCanvas(800, 500);
+    currentSketch = getDefaultSketch;
   }
   
   function draw() {
-    background(220);
-
-    push();
-    var welcome = "Welcome";
-    textSize(100);
-    text(welcome, 10, 100);
-    fill(0, 102, 153);
-    text(welcome, 10, 150);
-    fill(0, 102, 153, 51);
-    text(welcome, 10, 200);
-    pop();
-
-    push();
-    var intro = "Use the top bar menu to try out an exercise!";
-    let time = millis();
-    textSize(30);
-    text(intro, 10, 300);
-    pop();
+    currentSketch();
   }
