00b63838e76999a3edd022da92eeda7e61926ea4
Diadem Shoukralla <diadem.shoukralla@mail.utoronto.ca>
2019-09-01 18:50:20 -0400

    Set up the String Evolution exercise

--- sketch.js
+++ sketch.js
@@ -1,31 +1,24 @@
-var currentSketch;
-
-function getDefaultSketch(){
-  background(220);
-
-  push();
-  var welcome = "Welcome";
-  textSize(100);
-  text(welcome, 10, 100);
-  fill(0, 102, 153);
-  text(welcome, 10, 150);
-  fill(0, 102, 153, 51);
-  text(welcome, 10, 200);
-  pop();
-
-  push();
-  var intro = "Use the top bar menu to try out an exercise!";
-  let time = millis();
-  textSize(30);
-  text(intro, 10, 300);
-  pop();
-}
-
 function setup() {
     createCanvas(800, 500);
-    currentSketch = getDefaultSketch;
   }
   
   function draw() {
-    currentSketch();
+    background(220);
+
+    push();
+    var welcome = "Welcome";
+    textSize(100);
+    text(welcome, 10, 100);
+    fill(0, 102, 153);
+    text(welcome, 10, 150);
+    fill(0, 102, 153, 51);
+    text(welcome, 10, 200);
+    pop();
+  
+    push();
+    var intro = "Use the top bar menu to try out an exercise!";
+    let time = millis();
+    textSize(30);
+    text(intro, 10, 300);
+    pop();
   }
